63704021abe00275b9788020105a248b0d98df82
Garrett Broadhead <gwbroadhead@gmail.com>
2021-11-21 15:46:55 -0800

    format adjustments, style fixes, attempting to fuck button background color overflow

--- script.js
+++ script.js
@@ -4,11 +4,25 @@ const setDate = () => {
 }
 setDate()
 
-document.querySelector('.saveBtn').addEventListener("click", (event) => {
-  console.log(event.target)
+// EVENT ON CLICK FOR THE SAVE BUTTONS. SAVES TEXTAREA DATA TO LOCAL STORAGE WITH TIMEBLOCK IDS
+document.querySelectorAll('.saveBtn').forEach( e => e.addEventListener("click", (event) => {
   const hour = event.target.getAttribute("data-timeblock")
-  const input = event.target.previousElementSibling.value
-  console.log(hour)
-  console.log(input)
+  const input = document.getElementById(hour).value
+  // const input = event.target.previousElementSibling.value
+  localStorage.setItem(hour, input)
 
-})
+}))
+
+// 
+let currentHour = moment().hour()
+for (let i = 9; i <= 17; i++) {
+document.getElementById(i).value = localStorage.getItem(i)
+  if(currentHour > i){
+  document.getElementById(i).classList.add('past')
+
+  }else if(currentHour ===i){
+  document.getElementById(i).classList.add('present')
+  }else{
+  document.getElementById(i).classList.add('future')
+  }
+}
